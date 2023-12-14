5711060b474112cef98b1e95350be768d8ef290a
Mischa Sameli <m.sameli@backslash.ch>
2023-12-14 11:24:31 +0100

    removed IE support

--- src/fa.submit.spinner.js
+++ src/fa.submit.spinner.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 backslash - artists of new media (info@backslash.ch)
+ * Copyright (C) 2023 backslash - artists of new media (info@backslash.ch)
  * 
  * This work is licensed under the Creative Commons
  * Attribution 3.0 Unported License. To view a copy
@@ -10,12 +10,12 @@
  * no responsibility for any damage caused by using this plugin
  * or the documentation provided.
  *
- * Version 1.0.2
+ * Version 1.0.3
  */
 var faSpinner = {
 	defaults : {
 		'fontawesome_required' : true,
-		'fontawesome_url' : '//cdn-backslash.ch/lib/font-awesome/4.3/css/font-awesome.min.css',
+		'fontawesome_url' : 'https://ext.cdn-backslash.ch/lib/font-awesome/4.7/css/font-awesome.min.css',
 		'btn_text' : '<i class="fa fa-spinner fa-pulse" aria-hidden="true"></i> ',
 		'btn_replacestyle' : 'prepend', 
 		'btn_selector' : 'button[type=submit]',
@@ -23,15 +23,8 @@ var faSpinner = {
 		'timeout' : 100,
 		'ajax_listener' : false,
 		'use_attribute_definition' : false,
-		'use_ie_fallback' : false,
-		'ie_fallback_version' : 9,
-		'ie_fallback_btn_text' : 'loading…',
-		'ie_fallback_btn_replacestyle' : 'replace',
 		'debugmode' : false
 		},
-	constants : {
-		'isIEFallback' : false
-	},
 	init: function (config) {
 		var prop;
 		if (config) {
@@ -43,21 +36,13 @@ var faSpinner = {
 		},
 	onload : function () {
 		faSpinner.checkDependencies();
-		faSpinner.setIEFallback();
 		faSpinner.setButtonFunctions();
 		
 		if ( faSpinner.defaults.debugmode ){
 			faSpinner.log('faSpinner loaded');	
 		}
 		},
-	setIEFallback : function () {
-		if ( faSpinner.defaults.use_ie_fallback && faSpinner.getIEVersion() <= faSpinner.defaults.ie_fallback_version ) {
-			faSpinner.constants.isIEFallback = true;
-			// set defaults to IE properties
-			faSpinner.defaults.btn_text = faSpinner.defaults.ie_fallback_btn_text;
-			faSpinner.defaults.btn_replacestyle = faSpinner.defaults.ie_fallback_btn_replacestyle;
-		}
-	},
+	
 	setButtonFunctions : function () {
 		var topSelector 	= faSpinner.defaults.ajax_listener ? document : faSpinner.defaults.btn_selector;
 		var objectSelector 	= faSpinner.defaults.ajax_listener ? faSpinner.defaults.btn_selector : [];
@@ -88,16 +73,6 @@ var faSpinner = {
 					if ( typeof attrReplaceStyle != 'undefined' && faSpinner.isValidReplaceStyle(attrReplaceStyle) ) {
 						replaceStyle = attrReplaceStyle;
 					}
-					// ie fallbacks
-					var attrIEText = $e.data('faspinner-ie_fallback_btn_text');
-					if ( typeof attrReplaceStyle != 'undefined' && faSpinner.getIEVersion() <= faSpinner.defaults.ie_fallback_version ) {
-						newBtnContent = attrIEText;
-					}
-					var attrIEReplaceStyle = $e.data('faspinner-ie_fallback_btn_replacestyle');
-					if ( typeof attrReplaceStyle != 'undefined' && faSpinner.isValidReplaceStyle(attrIEReplaceStyle)
-						&& faSpinner.getIEVersion() <= faSpinner.defaults.ie_fallback_version ) {
-						replaceStyle = attrIEReplaceStyle;
-					}
 				} 
 			}
 			
@@ -106,15 +81,15 @@ var faSpinner = {
 				var existingContent = $(this).text();
 				var btnContent;
 				switch ( replaceStyle ) {
-			    	case 'prepend':
-			     		btnContent = newBtnContent.concat(existingContent);
-			      		break;
-			    	case 'append':
-			      		btnContent = existingContent.concat(newBtnContent);
-			      		break;    
-			    	default:
-			    		btnContent = newBtnContent
-			        	break;
+					case 'prepend':
+						btnContent = newBtnContent.concat(existingContent);
+						break;
+					case 'append':
+						btnContent = existingContent.concat(newBtnContent);
+						break;    
+					default:
+						btnContent = newBtnContent
+						break;
 				}
 				if ( faSpinner.defaults.debugmode ){
 				faSpinner.log(disableid,existingContent,btnContent);
@@ -122,7 +97,7 @@ var faSpinner = {
 				$e.attr('data-disableid', disableid).attr("label", existingContent);
 			
 				var firstRun = window.setTimeout(function(){faSpinner.setSpinner(disableid, btnContent)}, startDelay);
-          	}
+			}
 		});
 	},
 	isValidReplaceStyle : function ( s ) {
@@ -137,13 +112,13 @@ var faSpinner = {
 	checkDisabledButton : function (disableid) {
 		if( !$('[data-disableid="' + disableid +'"]').attr('disabled') ){
 			var $e = $('[data-disableid="' + disableid +'"]');			
-	    	$e.html($e.attr("label"));
-	    } else {
-	    	var nextRun = window.setTimeout(function(){faSpinner.checkDisabledButton(disableid)}, faSpinner.defaults.timeout);
-	    }
+			$e.html($e.attr("label"));
+		} else {
+			var nextRun = window.setTimeout(function(){faSpinner.checkDisabledButton(disableid)}, faSpinner.defaults.timeout);
+		}
 	},
 	checkDependencies : function () {
-		if ( faSpinner.defaults.fontawesome_required && !faSpinner.isFfontAwesomePresent() && faSpinner.getIEVersion() > 9 ) {
+		if ( faSpinner.defaults.fontawesome_required && !faSpinner.isFfontAwesomePresent() ) {
 			faSpinner.loadFontAwesome();
 			if ( faSpinner.defaults.debugmode ){
 				faSpinner.log('faSpinner not loaded, but required');
@@ -160,16 +135,6 @@ var faSpinner = {
 	},
 	loadFontAwesome : function () {
 		$('<link href="' + faSpinner.defaults.fontawesome_url + '" rel="stylesheet" mediatype="screen">').appendTo("head");
-	},
-	getIEVersion : function () {
-		var ua = window.navigator.userAgent;
-        var msie = ua.indexOf("MSIE ");
-        if (msie > 0) {
-        	return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)));
-        } else if ( !!navigator.userAgent.match(/Trident.*rv\:11\./) ){
-        	return 11;
-        }
-        return 99999;
 	},
 	log : function (msg) {
 		if ( window.console ) {
